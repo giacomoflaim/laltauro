0abd4a58d9de852d99e34782fa350c9536a598e1
Giacomo <giacomo.flaim.91@gmail.com>
2020-08-04 17:52:44 +0200

    update all

--- js/my-js.js
+++ js/my-js.js
@@ -12,3 +12,49 @@ $("#myCarousel img").click(function() {
     }
     console.log(number);
 });
+
+
+
+
+
+
+
+$(".easterEgg").mouseover(function () {
+    $("body").css("background", "rgb(120,250,128)");
+    $("body").css("background", "linear-gradient(330deg, rgba(120,250,128,0.7) 0%, rgba(0,243,255,1) 100%)");   
+});
+
+$(".easterEgg").mouseleave(function () {
+    $("body").css("background", "white"); 
+});
+
+
+
+
+$(document).ready(function () {
+
+  
+  $('.second-button').on('click', function () {      
+      $('.nav-chiuso').toggleClass('nav-aperto')
+      $('.animated-icon2').toggleClass('open');
+  });
+});
+
+
+/*
+var arrayImg = new Array();
+arrayImg[0] = "web_A.mp4";
+arrayImg[1] = "web_B.mp4";
+arrayImg[2] = "web_C.mp4";
+
+function getRandomImage(arrayImg,);
+
+function getRandomImage(imgAr, path) {
+    path = path || '/Users/giacomo/Documents/GitHub/laltauro/asset/video/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var myPlayer = '<source class="player" src="' + path + img + '" type="video/mp4">'      
+    document.write(myPlayer);
+};*/
+
+
